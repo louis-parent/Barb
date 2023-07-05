d161eb7659e57f565abd5fbb3eb96cccc881ba93
Louis Parent <contact@louis-parent.fr>
2023-07-05 23:13:53 +0200

    Adding pulling option to StareAtMouse

--- scripts/StareAtMouse.js
+++ scripts/StareAtMouse.js
@@ -1,5 +1,5 @@
 export default {
-	watch(element, deg) {
+	watch(element, deg, pulling) {
 		element.addEventListener("mousemove", event => {
 			const maxAngle = (deg || 15) + "deg";
 			const rect = event.target.getBoundingClientRect();
@@ -7,8 +7,8 @@ export default {
 			const centerX = rect.width / 2;
 			const centerY = rect.height / 2;
 
-			const vectorX = (event.offsetX / centerX) - 1;
-			const vectorY = ((event.offsetY / centerY) - 1) * -1;
+			const vectorX = ((event.offsetX / centerX) - 1) * (pulling === true ? -1 : 1);
+			const vectorY = ((event.offsetY / centerY) - 1) * (pulling === true ? 1 : -1);
 			
 			element.style.transform = `rotateY(calc(${vectorX} * ${maxAngle})) rotateX(calc(${vectorY} * ${maxAngle}))`;
 		});
