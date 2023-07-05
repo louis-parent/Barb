d7e187ea0af65116f046010701f70700fe29cc3c
Louis Parent <contact@louis-parent.fr>
2023-07-05 22:49:30 +0200

    Removing console.log from StareAtMouse

--- scripts/StareAtMouse.js
+++ scripts/StareAtMouse.js
@@ -1,23 +1,15 @@
 export default {
 	watch(element, deg) {
 		element.addEventListener("mousemove", event => {
+			const maxAngle = (deg || 15) + "deg";
 			const rect = event.target.getBoundingClientRect();
 
 			const centerX = rect.width / 2;
 			const centerY = rect.height / 2;
-			
-			console.log({
-				cursorX: event.offsetX, 
-				cursorY: event.offsetY,
-				centerX: centerX,
-				centerY: centerY
-			});
 
-			const maxAngle = (deg || 15) + "deg"
 			const vectorX = (event.offsetX / centerX) - 1;
 			const vectorY = ((event.offsetY / centerY) - 1) * -1;
 			
-			console.log(vectorX, vectorY);
 			element.style.transform = `rotateY(calc(${vectorX} * ${maxAngle})) rotateX(calc(${vectorY} * ${maxAngle}))`;
 		});
 								 
